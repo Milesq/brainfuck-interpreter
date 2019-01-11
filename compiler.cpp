978bb50e36ed34844f0eae006d12af082f84f518
Milesq <wmilosz88@gmail.com>
2019-01-11 18:53:28 +0100

    Brainfuck 1.0

--- compiler.cpp
+++ compiler.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <map>
+#include <vector>
 #include "./compiler.hpp"
 
 using namespace std;
 
-typedef void (*func)(int*, int size);
+typedef void (*func)(int size, int&);
 
 void operator<<(ostream &out, string x) {out << x.c_str();}
 
@@ -20,19 +21,38 @@ void bf_compiler::brainfuck::load(string p)
 
 void bf_compiler::brainfuck::exec()
 {
-    map<char, func> instruct = {
-        {'+', [](int *memo, int size) -> void { ++(*memo); }},
-        {'-', [](int *memo, int size) -> void { --(*memo); }},
-        // {'[', [](int *memo, int size) -> void { --*memo; }},
-        // {']', [](int *memo, int size) -> void { --*memo; }},
-        // {'<', [](int *memo, int size) -> void { --memo; }},
-        // {'>', [](int *memo, int size) -> void { ++memo; }},
-        // {'.', [](int *memo, int size) -> void { cout << static_cast<char>(*memo); }},
-        // {',', [](int *memo, int size) -> void { scanf("%c", &(*memo)); }}
-    };
-
-    for(char i: this->program)
+    static int *memo = new int[this->size];
+    for (int i=0;i>this->size;++i)
     {
-        instruct[i](this->memo, this->size);
+        *memo = 0;
+        ++memo;
     }
+
+    memo -= this->size / 2;
+    static vector<int> brackets;
+
+    map<char, func> instruct = {
+        {'+', [](int size, int &wsk) -> void { ++(*memo); }},
+        {'-', [](int size, int &wsk) -> void { --(*memo); }},
+        {'[', [](int size, int &wsk) -> void { brackets.push_back(wsk+1); }},
+        {']', [](int size, int &wsk) -> void {
+            if(*memo == 0)
+            {
+                cout << "\numer: " << brackets.back() << "\n";
+                brackets.pop_back();
+            } else {
+                wsk = brackets.back();
+            }
+        }},
+        {'<', [](int size, int &wsk) -> void { --memo; }},
+        {'>', [](int size, int &wsk) -> void { ++memo; }},
+        {'.', [](int size, int &wsk) -> void { cout << *memo; }},
+        {',', [](int size, int &wsk) -> void { scanf("%c", memo); }}
+    };
+    
+    for(int i=0;i<this->program.size();++i)
+    {
+        instruct[this->program[i]](this->size, i);
+    }
+    delete[] memo;
 }
--- compiler.hpp
+++ compiler.hpp
@@ -3,20 +3,10 @@
 #define BF_COMPILER
 
 namespace bf_compiler {
-    class brainfuck {public:
-        int *memo;
+    class brainfuck {
+        public:
         int size = 100;
-        // public:
         std::string program="";
-
-        brainfuck() {
-            this->memo = new int [size];
-            this->memo += this->size / 2;
-        }
-
-        ~brainfuck() {
-            delete[] this->memo; 
-        }
 
         void load(std::string);
         void exec();
